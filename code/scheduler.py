8903f7e0461be5072d4e44f417f914b45dc8e704
ohmymapy <145799700+ohmymapy@users.noreply.github.com>
2025-07-09 22:54:36 -0700

    adding more times for discord bot to scrape + notify avail appts

--- code/scheduler.py
+++ code/scheduler.py
@@ -24,8 +24,10 @@ schedule.every().day.at("07:00").do(run_notifier)
 schedule.every().day.at("09:00").do(run_notifier)
 schedule.every().day.at("12:00").do(run_notifier)
 schedule.every().day.at("14:00").do(run_notifier)
+schedule.every().day.at("17:00").do(run_notifier)
+schedule.every().day.at("00:00").do(run_notifier)
 
-print("🗓 Scheduler running... Will notify at 2:00 AM, 5:00 AM, 7:00 AM, 9:00AM, 12:00 PM, and 2:00PM.\n")
+print("🗓 Scheduler running... Will notify at 2:00 AM, 5:00 AM, 7:00 AM, 9:00AM, 12:00 PM, 2:00PM, 5:00PM and midnight.\n")
 
 # run FOREVER!!!
 while True:
